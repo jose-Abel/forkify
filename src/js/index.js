371db6127ab21439459bb83dee4be1041a6f242c
Bunbury <joseabel.ramirezfrontany@gmail.com>
2020-06-08 00:46:03 -0400

    create the searchView

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
-import Search from './models/Search';
+import Search from "./models/Search";
 
-import Recipe from './models/Recipe';
+import Recipe from "./models/Recipe";
 
-import * as searchView from './views/searchView';
+import * as searchView from "./views/searchView";
 
-import {elements, renderLoader, clearLoader} from './views/base';
+import * as recipeView from "./views/recipeView";
+
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /*
 Glogal state of the app
@@ -18,84 +20,84 @@ const state = {};
 
 //SEARCH CONTROLLER
 
-const controlSearch = async ()=> {
-    //1) Get query from view
-    const query = searchView.getInput(); //TODO
+const controlSearch = async () => {
+  //1) Get query from view
+  const query = searchView.getInput(); //TODO
 
-    if(query){
-        //2) New search object and add to state
-        state.search = new Search(query);
+  if (query) {
+    //2) New search object and add to state
+    state.search = new Search(query);
 
-        //3) Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
-        renderLoader(elements.searchRes);
+    //3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
-        try {
-            //4) Search for recipes
-            await state.search.getResults();
+    try {
+      //4) Search for recipes
+      await state.search.getResults();
 
-            //5) Render results on UI
-            clearLoader();
-            searchView.renderResults(state.search.result);
-        }
-        catch(err){
-            console.log('Something wrong with the search');
-            clearLoader();
-        }
+      //5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      console.log("Something wrong with the search");
+      clearLoader();
     }
-}
+  }
+};
 
-elements.searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    controlSearch();
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
 });
 
-elements.searchResPages.addEventListener('click', e=> {
-    const btn = e.target.closest('.btn-inline');
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
 
-    if(btn){
-        const goToPage = parseInt(btn.dataset.goto, 10);
-        searchView.clearResults();
-        searchView.renderResults(state.search.result, goToPage);
-    }
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
 
 //RECIPE CONTROLLER
 
 const controlRecipe = async () => {
-    //Get ID from url
-    const id = window.location.hash.replace('#', '');
-    console.log(id);
+  //Get ID from url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
 
-    if(id){
-        //Prepare UI for changes
+  if (id) {
+    //Prepare UI for changes
+    renderLoader(elements.recipe);
 
+    //Create new recipe object
+    state.recipe = new Recipe(id);
 
-        //Create new recipe object
-        state.recipe = new Recipe(id);
+    try {
+      //Get recipe data and parse ingredients
+      await state.recipe.getRecipe();
+      console.log(state.recipe.ingredients);
+      state.recipe.parseIngredients();
 
-        try{
-            //Get recipe data and parse ingredients
-            await state.recipe.getRecipe();
-            console.log(state.recipe.ingredients);
-            state.recipe.parseIngredients();
+      //Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
 
-            //Calculate servings and time
-            state.recipe.calcTime();
-            state.recipe.calcServings();
-
-            //Render recipe
-            console.log(state.recipe);
-        }
-
-        catch(error){
-            console.log('Something went wrong with the recipe');
-        }
+      //Render recipe
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
+    } catch (error) {
+      console.log("Something went wrong with the recipe");
     }
-}
+  }
+};
 
 // window.addEventListener('hashchange', controlRecipe);
 // window.addEventListener('load', controlRecipe);
 
-['haschange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+["haschange", "load"].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,26 +1,27 @@
 export const elements = {
-    searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field'),
-    searchRes: document.querySelector('.results'),
-    searchResList: document.querySelector('.results__list'),
-    searchResPages: document.querySelector('.results__pages')
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  searchRes: document.querySelector(".results"),
+  searchResList: document.querySelector(".results__list"),
+  searchResPages: document.querySelector(".results__pages"),
+  recipe: document.querySelector(".recipe")
 };
 
 export const elementStrings = {
-    loader: 'loader'
+  loader: "loader"
 };
 
 export const renderLoader = parent => {
-    // const loader = `
-    //     <div class="${elementStrings.loader}">
-    //         <svg>
-    //             <use href=img/icons.svg#icon-cw"></use>
-    //         </svg>
-    //     </div>
-    // `;
+  // const loader = `
+  //     <div class="${elementStrings.loader}">
+  //         <svg>
+  //             <use href=img/icons.svg#icon-cw"></use>
+  //         </svg>
+  //     </div>
+  // `;
 
-    //new loader added
-    const loader = `
+  //new loader added
+  const loader = `
     <div class="${elementStrings.loader}" id="loader-4">
       <span></span>
       <span></span>
@@ -28,15 +29,13 @@ export const renderLoader = parent => {
     </div>
     `;
 
-
-    parent.insertAdjacentHTML('afterbegin', loader);
+  parent.insertAdjacentHTML("afterbegin", loader);
 };
 
-
 export const clearLoader = () => {
-    const loader = document.querySelector(`.${elementStrings.loader}`);
+  const loader = document.querySelector(`.${elementStrings.loader}`);
 
-    if(loader){
-        loader.parentElement.removeChild(loader);
-    }
+  if (loader) {
+    loader.parentElement.removeChild(loader);
+  }
 };
