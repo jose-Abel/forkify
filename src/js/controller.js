4e24c4d949d67bf43bc7e834cc34ec3cee6e0301
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-13 17:26:26 -0400

    Create the paginationView.js to implement pagination in the app

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,8 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 
+import paginationView from './views/paginationView.js';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -38,16 +40,31 @@ const controlSearchResults = async function() {
 
     // 3) Render results
     resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+
   }
   catch(err) {
     console.log(err);
   }
 }
 
+const controlPagination = function(goToPage) {
+  // 1) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render nre pagination buttons
+  paginationView.render(model.state.search);
+
+}
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
 
   searchView.addHandlerSearch(controlSearchResults);
+
+  paginationView.addHandlerClick(controlPagination);
 }
 
 init();
