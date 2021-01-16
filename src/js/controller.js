ef83002e45029891ccd9a11e540ba3b802859786
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-16 11:28:51 -0400

    Create the addRecipeView and add the event listener to display the form to add a new recipe and have it call through the controller with the Publisher-Observer pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -89,6 +90,12 @@ const controlBookmarks = function() {
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlAddRecipe = function(newRecipe){
+  console.log(newRecipe);
+
+  // Upload the new recipe data
+}
+
 const init = function() {
   bookmarksView.addHandlerRender(controlBookmarks);
 
@@ -101,6 +108,8 @@ const init = function() {
   searchView.addHandlerSearch(controlSearchResults);
 
   paginationView.addHandlerClick(controlPagination);
+
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 }
 
 init();
