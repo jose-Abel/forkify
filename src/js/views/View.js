088a6cf946a60e27a19d5921d856545c4ae74cff
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-01-16 13:29:54 -0400

    Add documentation example to the render method in the View.js

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,15 @@ import icons from "url:../../img/icons.svg";
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe) 
+   * @param {boolean} [render=true] if false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this (Object) View instance
+   * @author Jose Abel Ramirez Frontany
+   * @todo Finish implementation
+   */
   render(data, render = true) {
     if(!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
 
